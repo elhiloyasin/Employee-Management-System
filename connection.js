14f9e4f99a9c64a41c0c3571a03d75b6c8caaaf8
Yasin Elhilo <elhiloyasin@gmail.com>
2021-02-16 00:09:22 -0500

    formated document

--- connection.js
+++ connection.js
@@ -2,22 +2,22 @@ const mysql = require('mysql2');
 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    // Your port; if not 3306
-    port: 3306,
-    // Your username
-    user: 'root',
-    // Your password
-    password: 'password',
-    database: 'employee_DB',
-  });
+  host: 'localhost',
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: 'root',
+  // Your password
+  password: 'password',
+  database: 'employee_DB',
+});
 
 
 
-  connection.connect(function(err){
- if(err) throw err
+connection.connect(function (err) {
+  if (err) throw err
 
-  });
+});
 
 
 module.exports = connection
